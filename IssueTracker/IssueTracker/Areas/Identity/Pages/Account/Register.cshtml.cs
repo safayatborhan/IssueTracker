521cd3a7a6af8531dddaf287378ae477c0031f48
safayat-zisan <zisan94268@gmail.com>
2019-10-24 23:06:57 +0600

    p

--- IssueTracker/IssueTracker/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ IssueTracker/IssueTracker/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -134,7 +134,7 @@ namespace IssueTracker.Areas.Identity.Pages.Account
                         values: new { userId = user.Id, code = code },
                         protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email", 
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
                     await _signInManager.SignInAsync(user, isPersistent: false);
